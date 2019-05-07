48a4f83dcee76a62b5a4279411ec0822b265b9eb
Hiroki Iida <hiroki.iida@retrieva.jp>
2019-05-07 17:39:12 +0900

    fix bugs

--- DTW/dtw.py
+++ DTW/dtw.py
@@ -3,13 +3,15 @@
 import numpy as np
 import pylab as plt
 import seaborn as sns
+from tqdm import tqdm
 import matplotlib.gridspec as gridspec
+from itertools import product
 
 
 def minVal(v1, v2, v3):
     if extract_value(v1) <= min(extract_value(v2), extract_value(v3)):
         return v1, 0
-    elif (v2) <= extract_value(v3):
+    elif extract_value(v2) <= extract_value(v3):
         return v2, 1
     else:
         return v3, 2
@@ -20,17 +22,17 @@ def calc_dtw(A, B):
     T = len(B)
 
     m = [[0 for j in range(T)] for i in range(S)]
-    m[0][0] = (np.linalg.norm(A[0], B[0]), (-1, -1))
+    m[0][0] = (norm(A[0], B[0]), (-1, -1))
     for i in range(1, S):
-        m[i][0] = (m[i-1][0][0] + np.linalg.norm(A[i], B[0]), (i-1, 0))
+        m[i][0] = (m[i-1][0][0] + norm(A[i], B[0]), (i-1, 0))
     for j in range(1, T):
-        m[0][j] = (m[0][j-1][0] + np.linalg.norm(A[0], B[j]), (0, j-1))
+        m[0][j] = (m[0][j-1][0] + norm(A[0], B[j]), (0, j-1))
 
     for i in range(1, S):
         for j in range(1, T):
             minimum, index = minVal(m[i-1][j], m[i][j-1], m[i-1][j-1])
             indexes = [(i-1, j), (i, j-1), (i-1, j-1)]
-            m[i][j] = (extract_value(minimum) + np.linalg.norm(A[i], B[j]), indexes[index])
+            m[i][j] = (extract_value(minimum) + norm(A[i], B[j]), indexes[index])
     return m
 
 
@@ -48,9 +50,9 @@ def multi_dtw(data):
 
     dtws = []
     
-    for i, (key1, key2) in enumerate(product(data.columns, data.columns)):
+    for i, (key1, key2) in tqdm(enumerate(product(data.columns, data.columns))):
         dtws.append(extract_value(calc_dtw(data[key1].get_values(),
-                                           data[key2].get_values())[-1][-1][0]))
+                                           data[key2].get_values())[-1][-1]))
 
     dtws = np.array(dtws).reshape(len(data.columns), -1)
     return dtws
@@ -62,6 +64,10 @@ def extract_value(value_index_pair):
 
 def extract_index(value_index_pair):
     return value_index_pair[1]
+
+
+def norm(a, b):
+    return (a - b)**2
 
 
 def backward(m):
